769e7a0a62fe82eec9638e23f0674a159ea62468
sriharivishnu <srihari.vishnu@gmail.com>
2019-06-23 01:47:16 -0400

    v3.3

--- app/build/generated/source/buildConfig/debug/com/corp/srihari/deca/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/corp/srihari/deca/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.corp.srihari.deca";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 7;
-  public static final String VERSION_NAME = "1.4.1";
+  public static final int VERSION_CODE = 8;
+  public static final String VERSION_NAME = "1.5.1";
 }
